7e49ba51cac185d978967e23c8454acaa2ab3b60
Your Name <you@example.com>
2020-04-13 18:41:11 +0300

    Init parX

---
+++ parx/src/test/java/com/parx/ParxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
